6dce13d9f80fffbc0b2378816441d36af3f190d0
agungramadhan91 <agungramadhans.work@gmail.com>
2025-06-01 02:49:51 +0700

    p4 done in termux

--- p4-rock-scissors-paper/p4_main.py
+++ p4-rock-scissors-paper/p4_main.py
@@ -4,4 +4,20 @@ options = ["🪨","📜","✂️"]
 
 choose_type = input("Choose number: 1-Rock, 2-Paper, 3-Scissors: ")
 
-print(choose_type)
+user_choice = int(choose_type) - 1
+comp_choice = random.randint(0,2)
+
+print(type(user_choice))
+print(f"User choice: {options[user_choice]}")
+print(f"Computer choice: {options[comp_choice]}")
+
+if user_choice == comp_choice:
+    print("Draw!!!")
+elif user_choice == 0 and comp_choice == 2:
+    print("You Win!")
+elif user_choice == 1 and comp_choice == 0:
+    print("You Win!")
+elif user_choice == 2 and comp_choice == 1:
+    print("You Win!")
+else:
+    print("You Lose!")
